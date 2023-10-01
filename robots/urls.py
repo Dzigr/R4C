f95db1e1099abe37e0467e05cfea0c6aaf8adfe4
dzigr <dmbrag@gmail.com>
2023-10-01 19:52:46 +0300

    Add excel creating feature

--- robots/urls.py
+++ robots/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
-from .views import CreateRobotView
+from .views import CreateRobotView, DownloadExcelView
 
 urlpatterns = [
     path('add/', csrf_exempt(CreateRobotView.as_view()), name='add_robot'),
+    path('get_stat/', csrf_exempt(DownloadExcelView.as_view()), name='get_excel_data'),
 ]
